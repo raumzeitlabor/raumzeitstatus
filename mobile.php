31e095276bbb3be8536f443c29cbf029d941aefd
Tim Weber <scy@scytale.name>
2010-04-24 04:21:27 +0800

    XHTML fixes.

--- mobile.php
+++ mobile.php
@@ -15,19 +15,19 @@ if (isset($_POST['set'])) {
 <head>
 	<title>RaumZeitLabor Raumstatus</title>
 	<meta name="viewport" content="width=device-width; initial-scale=1.0; maximum-scale=1.0; user-scalable=0;" />
-	<link rel="apple-touch-icon" href="rzl.png">
+	<link rel="apple-touch-icon" href="rzl.png" />
 </head>
 <body style="min-height: 420px;" onload="setTimeout(function() { window.scrollTo(0, 1) }, 100);">
-	<span style="text-align: center;"><img width="150px" style="display: block; margin-left: auto; margin-right: auto;" src="status.php">
+	<span style="text-align: center;"><img width="150px" style="display: block; margin-left: auto; margin-right: auto;" src="status.php" />
 	<form action="mobile.php" method="post">
 		<?php
 			$roomStatus = (int)file_get_contents('room');
 			switch ($roomStatus) {
 			case 1:
-				$info = '<input style="font-size: 20px;" type="submit" value="Raum schlie&szlig;en"><input type="hidden" value="0" name="set">';
+				$info = '<input style="font-size: 20px;" type="submit" value="Raum schlie&szlig;en" /><input type="hidden" value="0" name="set" />';
 				break;
 			case 0:
-				$info = '<input style="font-size: 20px;" type="submit" value="Raum &ouml;ffnen"><input type="hidden" value="1" name="set">';
+				$info = '<input style="font-size: 20px;" type="submit" value="Raum &ouml;ffnen" /><input type="hidden" value="1" name="set" />';
 				break;
 			}
 			echo $info;
