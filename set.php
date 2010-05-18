634580eda791fd0aac76c4c1a8ab3350626ae0c2
Raphael Michel <webmaster@raphaelmichel.de>
2010-05-19 01:07:24 +0800

    missing } added, useless fopen() removed

--- set.php
+++ set.php
@@ -1,16 +1,14 @@
 <?php
-
 if (isset($_GET['set'])) {
 	$set = (int)$_GET['set'];
-	$handler = fopen("room" , "w+");
 	switch ($set) {
-	case 0:
-	case 1:
-	case 2:
-		file_put_contents('room', $set);
-		break;
-	default:
-		echo "Fehler. Status konnte nicht gesetzt werden.";
+		case 0:
+		case 1:
+		case 2:
+			file_put_contents('room', $set);
+			break;
+		default:
+			echo "Fehler. Status konnte nicht gesetzt werden.";
+	}
 }
-
 ?>
