4829e7b9839142320925c5cbcea8669ee9c11b66
Michael Stapelberg <michael@stapelberg.de>
2010-08-06 01:15:01 +0200

    status.php zum direkten Einbinden des Bilds durch einen Redirect gelöst

---
+++ status.php
@@ -0,0 +1,20 @@
+<?php
+/* von http://php.net/manual/de/function.header.php */
+header("Cache-Control: no-cache, must-revalidate");
+header("Expires: Sat, 26 Jul 1997 05:00:00 GMT");
+
+$roomStatus = file_get_contents('http://scytale.name/files/tmp/rzlstatus.txt');
+switch ($roomStatus) {
+case '1':
+	$bild = 'images/green.png';
+	break;
+case '0':
+	$bild = 'images/red.png';
+	break;
+default:
+	$bild = 'images/orange.png';
+	break;
+}
+
+header("Location: $bild");
+?>
