29cb1b3aef95957238ae3ab8a19da60dd92aa5f6
Michael Stapelberg <michael@stapelberg.de>
2010-08-06 00:54:09 +0200

    Header hinzugefügt um Caching zu verhindern

--- index.php
+++ index.php
@@ -1,3 +1,8 @@
+<?php
+/* von http://php.net/manual/de/function.header.php */
+header("Cache-Control: no-cache, must-revalidate");
+header("Expires: Sat, 26 Jul 1997 05:00:00 GMT");
+?>
 <html>
 <head>
 <title>RaumZeitLabor: Status</title>
