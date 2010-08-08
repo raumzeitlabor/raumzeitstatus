a8f6cdc775c687f5fdf808dea631ae22c8ed9b55
Felix Arndt <kontakt@felixarndt.de>
2010-08-08 15:55:03 +0200

    Bugfix: Fehlends trim() in status.php ergänzt

--- status.php
+++ status.php
@@ -3,7 +3,7 @@
 header("Cache-Control: no-cache, must-revalidate");
 header("Expires: Sat, 26 Jul 1997 05:00:00 GMT");
 
-$roomStatus = file_get_contents('http://scytale.name/files/tmp/rzlstatus.txt');
+$roomStatus = trim(file_get_contents('http://scytale.name/files/tmp/rzlstatus.txt'));
 switch ($roomStatus) {
 case '1':
 	$bild = 'images/green.png';
