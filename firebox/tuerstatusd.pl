f4708cf090ab92f1c8d27969c8c8ebfc8e03d6df
Simon Elsbrock <simon@iodev.org>
2013-01-26 21:28:40 +0100

    add current version of tuerstatusd.pl

--- firebox/tuerstatusd.pl
+++ firebox/tuerstatusd.pl
@@ -2,22 +2,36 @@
 # vim:ts=4:sw=4:expandtab
 # Script, welches vom Hausbus den Türstatus liest und im JSON/rrd/MySQL ablegt
 
+use strict;
+use warnings;
+
 use JSON::XS;
 use RRDTool::OO;
 use DBIx::Simple;
 use Data::Dumper;
 use Net::INET6Glue::INET_is_INET6;
-use IO::All;
 use lib qw(.);
 use lib qw(ae-http-stream-lib);
 use AnyEvent;
 use AnyEvent::HTTP;
 use sqlconfig;
 use MIME::Base64;
+use POSIX qw(strftime);
+use IO::Handle;
 
 use v5.10;
 
 use AnyEvent::HTTP::Stream;
+
+sub prefix {
+	strftime("%x %X - ", gmtime())
+}
+
+$| = 1;
+
+open(my $vf_log, '>>', '/tmp/vflog.log');
+$vf_log->autoflush(1);
+say $vf_log prefix() . "tuerstatusd.pl starting";
 
 my $cv = AE::cv;
 my $stream = AnyEvent::HTTP::Stream->new(
@@ -25,10 +39,22 @@ my $stream = AnyEvent::HTTP::Stream->new(
 	on_data => sub {
 		my ($data) = @_;
 		my $pkt = decode_json($data);
+		say $data;
 		return unless exists $pkt->{payload};
+		if ($pkt->{payload} =~ /^SRAW /) {
+			my $status = $pkt->{payload};
+			$status =~ s/^SRAW //g;
+			my ($b1, $b2, $b3, $b4) = unpack("CCCC", $status);
+			say "raw status bytes = $b1, $b2, $b3, $b4";
+			return;
+		}
+		return if ($pkt->{payload} !~ /^STAT / && $pkt->{payload} !~ /^EE/ && $pkt->{payload} !~ /^X / && $pkt->{payload} !~ /^VF /);
+		if ($pkt->{payload} =~ /^VF /) {
+			say $vf_log prefix() . "VF: $pkt->{payload}";
+		}
+		say "payload = " . Dumper($pkt->{payload});
 		return unless $pkt->{payload} =~ /^STAT /;
 		my ($status) = ($pkt->{payload} =~ /^STAT (.+)/);
-		say "new status $status";
 		my $statuscode;
 		if ($status eq 'open') {
 			$statuscode = '1';
@@ -37,7 +63,7 @@ my $stream = AnyEvent::HTTP::Stream->new(
 		} else {
 			$statuscode = '?';
 		}
-		say "translates to code $statuscode";
+		say "status $status (code $statuscode)";
 		new_status($statuscode);
 	},
     on_error => sub {
@@ -71,7 +97,7 @@ sub new_status {
 		  };
 
 
-	my $rrd = RRDTool::OO->new(file => "status-tuer.rrd");
+	my $rrd = RRDTool::OO->new(file => "/tmp/rrd/status-tuer.rrd");
 	if ($output eq '1' or $output eq '0') {
 		say "updating in rrd";
 	$rrd->update(values => { 'tuer' => $output } );
