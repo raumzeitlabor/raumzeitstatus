6d02557383e1357b79bf80354bec17a189fd5890
Maik Fischer <maikf@qu.cx>
2014-04-28 23:22:23 +0200

    unifi-check.pl: more cosmetic changes

--- firebox/unifi-check.pl
+++ firebox/unifi-check.pl
@@ -26,15 +26,15 @@ my $db = DBIx::Simple->connect(
     $sqlconfig::pass
 );
 
-my $UNIFI_CTRL = "https://unifi.vm:8443";
+my $UNIFI_CTRL = 'https://unifi.vm:8443';
 
 # login
-my $cv        = AE::cv;
+my $cv = AE::cv;
 my $cookiejar = {};
 
 my $login_body = 'login=Login&username=' .  url_encode_utf8('foo') . '&password=' . url_encode_utf8('bar');
 my $login_request = http_request(
-    POST       => $UNIFI_CTRL . "/login",
+    POST       => "$UNIFI_CTRL/login",
     timeout    => 3,
     recurse    => 0,
     persistent => 1,
@@ -42,23 +42,23 @@ my $login_request = http_request(
     tls_ctx    => { sslv2 => 0, sslv3 => 1, tlsv1 => 0 },
     body       => $login_body,
     headers => {
-        "Content-Type" => "application/x-www-form-urlencoded",
+        'Content-Type' => 'application/x-www-form-urlencoded',
     },
     sub {
         my ($partial_body, $hdr) = @_;
-        $cv->send($hdr->{Status} eq "302");
+        $cv->send($hdr->{Status} == 302);
     }
 );
 
-croak "wrong credentials " if (!$cv->recv);
+croak('wrong credentials') unless $cv->recv;
 undef $login_request;
 
-INFO "logged in";
+INFO('logged in');
 
 # fetch stations
 $cv = AE::cv;
 my $station_request = http_request(
-    GET        => $UNIFI_CTRL . "/api/stat/sta",
+    GET        => "$UNIFI_CTRL/api/stat/sta",
     timeout    => 3,
     persistent => 1,
     cookie_jar => $cookiejar,
@@ -71,7 +71,7 @@ my $station_request = http_request(
 
 my $stations = $cv->recv;
 undef $station_request;
-INFO "" . @{ $stations->{data} } . " stations connected to AP";
+INFO(scalar @{ $stations->{data} } . ' stations connected to AP');
 
 $db->begin_work;
 $db->query('DELETE FROM leases');
@@ -79,10 +79,11 @@ $db->query('DELETE FROM leases');
 my @macs = ();
 for my $station (@{ $stations->{data} }) {
     # first_seen, last_seen, uptime, oui, assoc_time
-    my $state = ($station->{powersave_enabled} ? "SLEEPING" : "  ACTIVE");
-    INFO
-        sprintf("%10s | %8s | %25s | %12s | %8s", $station->{mac}, $state,
-        $station->{hostname}, $station->{ip}, $station->{oui});
+    my $state = ($station->{powersave_enabled} ? 'SLEEPING' : '  ACTIVE');
+    INFO(sprintf '%10s | %8s | %25s | %12s | %8s',
+            $station->{mac}, $state, $station->{hostname},
+            $station->{ip}, $station->{oui}
+    );
 
     push @macs, $station->{mac} if (time - $station->{last_seen} < 60);    # XXX
 
@@ -114,23 +115,23 @@ my @tmp = $db->select('devices', 'handle', {
 })->flat;
 
 my @laboranten = keys %{ { map { $_ => 1 } @tmp } };
-INFO "laboranten: " . join ", ", @laboranten;
+INFO('laboranten: ' . join ', ', @laboranten);
 
 $cv = AE::cv;
 my $logout_request = http_request(
-    GET        => $UNIFI_CTRL . "/logout",
+    GET        => "$UNIFI_CTRL/logout",
     cookie_jar => $cookiejar,
-    sub { $cv->send; }
+    $cv
 );
 
 $cv->recv;
-INFO "logged out";
+INFO('logged out');
 
-INFO "posting update";
+INFO('posting update');
 
-my $username = "bar";
-my $password = "foo";
-my $auth     = "Basic " . MIME::Base64::encode("$username:$password", '');
+my $username = 'bar';
+my $password = 'foo';
+my $auth = 'Basic ' . MIME::Base64::encode("$username:$password", '');
 
 my $done = AnyEvent->condvar;
 http_post(
@@ -138,7 +139,7 @@ http_post(
     encode_json(
         {
             details => {
-                geraete    => "" . @{ $stations->{data} },
+                geraete    => scalar @{ $stations->{data} },
                 laboranten => \@laboranten,
             }
         }
@@ -152,4 +153,4 @@ http_post(
     }
 );
 
-INFO "DONE (" . $done->recv . ")";
+INFO('DONE (' . $done->recv . ')');
