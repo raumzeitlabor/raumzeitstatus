7b2fe6408e4241051be300bbdcd714206634e21d
Maik Fischer <maikf@qu.cx>
2014-04-29 18:53:01 +0200

    status-graph.sh: make source files configureble

--- firebox/status-graph.sh
+++ firebox/status-graph.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
+TUER_RRD="status-tuer.rrd"
+GERAETE_RRD="status-geraete.rrd"
+
 /usr/bin/rrdtool graph /var/www/status-1day.png -s 'now-1day' -w 800 -h 400 -E \
-	DEF:tuer=status-tuer.rrd:tuer:LAST  \
-	DEF:geraete=status-geraete.rrd:geraete:LAST \
+	DEF:tuer=$TUER_RRD:tuer:LAST  \
+	DEF:geraete=$GERAETE_RRD:geraete:LAST \
 	CDEF:g=geraete,geraete,FLOOR,-,0.5,-,geraete,geraete,FLOOR,-,0.5,-,ABS,+,geraete,CEIL,geraete,FLOOR,IF \
 	AREA:tuer#e2ffe2 \
 	LINE2:tuer#77ff77:"Türstatus" \
@@ -11,8 +14,8 @@
 /usr/bin/rrdtool graph /var/www/status-1week.png \
 	-t 'Status des RaumZeitLabors über die letzte Woche' \
 	-s 'now-1week' -w 800 -h 400 -E \
-	DEF:tuer=status-tuer.rrd:tuer:LAST  \
-	DEF:geraete=status-geraete.rrd:geraete:LAST \
+	DEF:tuer=$TUER_RRD:tuer:LAST  \
+	DEF:geraete=$GERAETE_RRD:geraete:LAST \
 	CDEF:g=geraete,geraete,FLOOR,-,0.5,-,geraete,geraete,FLOOR,-,0.5,-,ABS,+,geraete,CEIL,geraete,FLOOR,IF \
 	"CDEF:p=tuer,100,*" \
 	AREA:tuer#e2ffe2 \
