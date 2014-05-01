11132143118c52fb94a544d8e0983568d212225d
Maik Fischer <maikf@qu.cx>
2014-05-01 13:06:07 +0200

    raumstatus-update: move program logic from Update.pm to ../Status.pm

---
+++ raumstatus-update/lib/RaumZeitLabor/Status.pm
@@ -0,0 +1,171 @@
+package RaumZeitLabor::Status;
+
+use 5.014;
+use strict;
+use warnings FATAL => 'all';
+
+use RaumZeitLabor::Status::BenutzerDB;
+use RaumZeitLabor::Status::Update;
+
+use EV;
+use Coro;
+use AnyEvent;
+# initialize AnyEvent as soon as possible to make
+# sure integration of EV/Coro/AnyEvent works as expected.
+BEGIN { AnyEvent::detect; }
+
+has 'config' => (
+    is => 'ro',
+    default => sub { _load_config("$ENV{HOME}/raumstatus_config.json") },
+);
+
+=head1 NAME
+
+RaumZeitLabor::Status - The great new RaumZeitLabor::Status!
+
+=head1 VERSION
+
+Version 0.0.001
+
+=cut
+
+our $VERSION = '0.000.001';
+
+
+=head1 SYNOPSIS
+
+Quick summary of what the module does.
+
+Perhaps a little code snippet.
+
+=cut
+
+sub run {
+    my ($self) = @_;
+    my $db = RaumZeitLabor::Status::BenutzerDB->new(
+        config => $self->config->{db}
+    );
+
+    RaumZeitLabor::Status::Update::unifi_login();
+
+    my $stations = RaumZeitLabor::Status::Update::unifi_stations();
+
+    RaumZeitLabor::Status::Update::station_debuginfo($_) for @$stations;
+    $db->update_leases($stations);
+
+    my $status = $db->internal_status;
+
+    RaumZeitLabor::Status::Update::post_status_update($status);
+}
+
+sub _load_config {
+    my ($file) = @_;
+
+    open my $fh, '<', $file
+        or die "Could not open config file: $file\n";
+
+    # allow comments and trailing commata in lists
+    my $json = JSON::XS->new->relaxed(1);
+
+    my $config = $json->decode(do { local $/; <$fh> });
+
+    for my $module (qw/unifi status db/) {
+        die "config for $module does not exist."
+            unless exists $config->{$module};
+
+        die "config for $module is incomplete"
+            unless 3 == grep { $config->{$module}{$_} }
+                            qw/uri user pass/;
+    }
+
+    return $config;
+}
+
+1;
+__END__
+
+=head1 AUTHOR
+
+Maik Fischer, C<< <maikf at qu.cx> >>
+
+=head1 BUGS
+
+Please report any bugs or feature requests to C<bug-raumzeitlabor-status-update at rt.cpan.org>, or through
+the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=RaumZeitLabor-Status-Update>.  I will be notified, and then you'll
+automatically be notified of progress on your bug as I make changes.
+
+
+
+
+=head1 SUPPORT
+
+You can find documentation for this module with the perldoc command.
+
+    perldoc RaumZeitLabor::Status::Update
+
+
+You can also look for information at:
+
+=over 4
+
+=item * RT: CPAN's request tracker (report bugs here)
+
+L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=RaumZeitLabor-Status-Update>
+
+=item * AnnoCPAN: Annotated CPAN documentation
+
+L<http://annocpan.org/dist/RaumZeitLabor-Status-Update>
+
+=item * CPAN Ratings
+
+L<http://cpanratings.perl.org/d/RaumZeitLabor-Status-Update>
+
+=item * Search CPAN
+
+L<http://search.cpan.org/dist/RaumZeitLabor-Status-Update/>
+
+=back
+
+
+=head1 ACKNOWLEDGEMENTS
+
+
+=head1 LICENSE AND COPYRIGHT
+
+Copyright 2014 Maik Fischer.
+
+This program is distributed under the (Revised) BSD License:
+L<http://www.opensource.org/licenses/BSD-3-Clause>
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions
+are met:
+
+* Redistributions of source code must retain the above copyright
+notice, this list of conditions and the following disclaimer.
+
+* Redistributions in binary form must reproduce the above copyright
+notice, this list of conditions and the following disclaimer in the
+documentation and/or other materials provided with the distribution.
+
+* Neither the name of Maik Fischer's Organization
+nor the names of its contributors may be used to endorse or promote
+products derived from this software without specific prior written
+permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+"AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+
+=cut
+
+# vim: set ts=4 sw=4 sts=4 expandtab:
--- raumstatus-update/lib/RaumZeitLabor/Status/Update.pm
+++ raumstatus-update/lib/RaumZeitLabor/Status/Update.pm
@@ -4,17 +4,7 @@ use 5.014;
 use strict;
 use warnings FATAL => 'all';
 
-use RaumZeitLabor::Status::BenutzerDB;
-
 use Carp;
-use Time::Piece;
-
-use EV;
-use Coro;
-use AnyEvent;
-# initialize AnyEvent as soon as possible to make
-# sure integration of EV/Coro/AnyEvent works as expected.
-BEGIN { AnyEvent::detect; }
 
 use AnyEvent::HTTP;
 
@@ -23,47 +13,9 @@ use JSON::XS;
 use HTTP::Request::Common ();
 use MIME::Base64;
 
-=head1 NAME
-
-RaumZeitLabor::Status::Update - The great new RaumZeitLabor::Status::Update!
-
-=head1 VERSION
-
-Version 0.0.001
-
-=cut
-
-our $VERSION = '0.000.001';
-
-
-=head1 SYNOPSIS
-
-Quick summary of what the module does.
-
-Perhaps a little code snippet.
-
-=cut
-
 Log::Log4perl->easy_init($DEBUG);
 
-my $CONFIG = load_config("$ENV{HOME}/raumstatus_config.json");
-
 my $UNIFI_CTRL = $CONFIG->{unifi}{uri};
-
-sub run {
-    my $db = RaumZeitLabor::Status::BenutzerDB->new(config => $CONFIG->{db});
-
-    unifi_login();
-
-    my $stations = unifi_stations();
-
-    station_debuginfo($_) for @$stations;
-    $db->update_leases($stations);
-
-    my $status = $db->internal_status;
-
-    post_status_update($status);
-}
 
 sub station_debuginfo {
     my ($station) = @_;
@@ -186,114 +138,4 @@ sub unifi_request {
     return Coro::rouse_wait($wait);
 }
 
-sub load_config {
-    my ($file) = @_;
-
-    open my $fh, '<', $file
-        or die "Could not open config file: $file\n";
-
-    # allow comments and trailing commata in lists
-    my $json = JSON::XS->new->relaxed(1);
-
-    my $config = $json->decode(do { local $/; <$fh> });
-
-    for my $module (qw/unifi status db/) {
-        die "config for $module does not exist."
-            unless exists $config->{$module};
-
-        die "config for $module is incomplete"
-            unless 3 == grep { $config->{$module}{$_} }
-                            qw/uri user pass/;
-    }
-
-    return $config;
-}
-
 1; # End of RaumZeitLabor::Status::Update
-__END__
-
-=head1 AUTHOR
-
-Maik Fischer, C<< <maikf at qu.cx> >>
-
-=head1 BUGS
-
-Please report any bugs or feature requests to C<bug-raumzeitlabor-status-update at rt.cpan.org>, or through
-the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=RaumZeitLabor-Status-Update>.  I will be notified, and then you'll
-automatically be notified of progress on your bug as I make changes.
-
-
-
-
-=head1 SUPPORT
-
-You can find documentation for this module with the perldoc command.
-
-    perldoc RaumZeitLabor::Status::Update
-
-
-You can also look for information at:
-
-=over 4
-
-=item * RT: CPAN's request tracker (report bugs here)
-
-L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=RaumZeitLabor-Status-Update>
-
-=item * AnnoCPAN: Annotated CPAN documentation
-
-L<http://annocpan.org/dist/RaumZeitLabor-Status-Update>
-
-=item * CPAN Ratings
-
-L<http://cpanratings.perl.org/d/RaumZeitLabor-Status-Update>
-
-=item * Search CPAN
-
-L<http://search.cpan.org/dist/RaumZeitLabor-Status-Update/>
-
-=back
-
-
-=head1 ACKNOWLEDGEMENTS
-
-
-=head1 LICENSE AND COPYRIGHT
-
-Copyright 2014 Maik Fischer.
-
-This program is distributed under the (Revised) BSD License:
-L<http://www.opensource.org/licenses/BSD-3-Clause>
-
-Redistribution and use in source and binary forms, with or without
-modification, are permitted provided that the following conditions
-are met:
-
-* Redistributions of source code must retain the above copyright
-notice, this list of conditions and the following disclaimer.
-
-* Redistributions in binary form must reproduce the above copyright
-notice, this list of conditions and the following disclaimer in the
-documentation and/or other materials provided with the distribution.
-
-* Neither the name of Maik Fischer's Organization
-nor the names of its contributors may be used to endorse or promote
-products derived from this software without specific prior written
-permission.
-
-THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
-"AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
-LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
-A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
-OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
-SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
-LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
-DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
-THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
-OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-
-
-=cut
-
-# vim: set ts=4 sw=4 sts=4 expandtab:
